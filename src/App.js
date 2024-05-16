80def11338535c0d6fc8e19b20ef9be58849b159
Ecilia Larissa <ecilialarissa@gmail.com>
2024-05-16 17:18:03 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'; 
+import imgout from './assets/asset-03.png';
+import imgin from './assets/asset-02.png';
+import img from './assets/asset-01.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+  <>
+    <div className="maindiv"> 
+
+    <div className="espiral">  
+
+    <div className="outer">
+      <img src={imgout} alt=""/>
+      <div className="inner">
+        <img src={imgin}/>
+      <div className="inside">
+        <img src={img}/>
+      </div>
+      </div>
     </div>
-  );
+    </div>
+
+   
+    <div className="title">
+      <h1>BEM-VINDO <span> ao Futuro</span> </h1>
+      <button className="font-btn">
+       Começar
+      </button>
+    </div>
+    </div>
+
+   
+  </>
+
+
+  )
 }
 
 export default App;
